0ce36455d8e0efea1f1d6b5159842d0d12150be9
featzai55 <77040252+featzai55@users.noreply.github.com>
2021-06-27 01:43:25 +0530

    Update creds.py

--- creds.py
+++ creds.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
 class cred():
-    BOT_TOKEN = os.getenv("BOT_TOKEN") #From botfather
-    API_ID = os.getenv("API_ID")       #"Get this value from my.telegram.org! Please do not steal"
-    API_HASH = os.getenv("API_HASH")   #"Get this value from my.telegram.org! Please do not steal"
+    BOT_TOKEN = os.getenv("1813651489:AAEaFf4SmyS61j8PksiTdSdVtX54l74OKzM") #From botfather
+    API_ID = os.getenv("2995136")       #"Get this value from my.telegram.org! Please do not steal"
+    API_HASH = os.getenv("3c3766c8df153afb9e5ebfe7314af305")   #"Get this value from my.telegram.org! Please do not steal"
